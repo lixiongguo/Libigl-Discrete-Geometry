406b1855390e0900781b575cff8d7288c269f5fd
lixiongguo <lixiongguox@163.com>
2025-09-29 20:29:07 +0800

    增加arap

---
+++ interactive_arap_main.cpp
@@ -0,0 +1,15 @@
+///**
+// * Project: Interactive ARAP
+// * File:    main.cpp
+// * Authors: Batuhan Erden, Cansu Yildirim, Anas Shahzad, Alexander Epple
+// */
+//
+//#include "include/Mesh.h"
+//
+//int main(int argc, char *argv[]) {
+//   Mesh mesh("D:/MyDocs/GeometryLab/Models/resources/models/cactus/cactus_small.off");
+//   mesh.launchViewer();
+//
+//   return 0;
+//}
+//
