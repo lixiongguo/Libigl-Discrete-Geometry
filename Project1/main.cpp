bd35c632004d5a33056e48566d38b2c32c1acbc6
lixiongguo <lixiongguox@163.com>
2025-10-16 20:58:05 +0800

    增加了一些QuadCover的内容

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,7 +1,6 @@
 #include <igl/read_triangle_mesh.h>
 #include <igl/principal_curvature.h>
 #include <igl/opengl/glfw/Viewer.h>
-#include <igl/per_vertex_normals.h>
 #include <Eigen/Core>
 #include <iostream>
 #include <igl/jet.h>
@@ -24,10 +23,6 @@ int main(int argc, char *argv[])
     // 可选参数：number of iterations for smoothing (默认5)
     igl::principal_curvature(V, F, PD1, PD2, PV1, PV2, 5);
 
-    //// 可选：计算顶点法向用于可视化
-    //Eigen::MatrixXd N;
-    //igl::per_vertex_normals(V, F, N);
-
 
     Eigen::VectorXd K = PV1.array() * PV2.array();//K = k1*k2 高斯曲率
     Eigen::MatrixXd C;
@@ -39,7 +34,6 @@ int main(int argc, char *argv[])
     viewer.data().set_colors(C);
 
     //viewer.data().set_normals(N);
-
   
 
     //// 可选：显示主方向向量（作为线段）
@@ -54,6 +48,37 @@ int main(int argc, char *argv[])
     //    viewer.data().add_edges(P_start.row(i), P_end1.row(i), Eigen::RowVector3d(1, 0, 0)); // 红色
     //    viewer.data().add_edges(P_start.row(i), P_end2.row(i), Eigen::RowVector3d(0, 1, 0)); // 绿色
     //}
+
+
+   //     // 2. 计算每个面的法向量
+   // MatrixXd FN;
+   // MatrixXd VN;
+   // igl::per_face_normals(V, F, FN);
+   // igl::per_vertex_normals(V, F, VN);
+   // const double  nScale =  0.1;
+
+   // MatrixXd face_centers(n_faces, 3);
+   // for (int i = 0; i < n_faces; ++i) {
+   //     face_centers.row(i) = (V.row(F(i, 0))+(V.row(F(i, 1))) + (V.row(F(i, 2))))/3.0;
+   // }
+   // 
+   // 
+
+   // igl::opengl::glfw::Viewer viewer;
+
+   // viewer.data().set_mesh(V, F);
+   // viewer.data().set_normals(VN);
+
+   // for (int i = 0; i < n_faces; ++i)
+   // {
+   //     viewer.data().add_edges(face_centers.row(i),face_centers.row(i)+nScale*FN.row(i),Eigen::RowVector3d(1,0,0));
+   // }
+
+   ///* for (int i = 0; i < V.rows(); i++)
+   // {
+   //     viewer.data().add_edges(V.row(i),V.row(i)+ VN.row(i)*nScale,Eigen::RowVector3d(1,0,0));
+   // }*/
+   // viewer.launch();
 
     viewer.launch();
     return 0;
